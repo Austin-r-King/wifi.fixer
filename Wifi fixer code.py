b0376e36b0394c8a987a9e79667eaee52cc5c226
Austin-r-King <de.king.hi@gmail.com>
2021-03-15 10:06:04 -0400

    main python file

--- Wifi fixer code.py
+++ Wifi fixer code.py
@@ -1,6 +1,10 @@
 
 # Austin, Aiden
 
-print("hello world")
+import time
 
+#gets the curent time
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+#print(current_time) to get current time when needed
 
