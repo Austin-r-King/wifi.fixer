cd6c8832d431eda00d885888337dd44af128b380
Austin-r-King <de.king.hi@gmail.com>
2021-03-16 08:30:30 -0400

    main python file

--- Wifi fixer code.py
+++ Wifi fixer code.py
@@ -1,8 +1,13 @@
 # Austin, Aiden
+
 import time
+
 disconnect_log = []
 # gets the current time
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 disconnect_log.append(current_time)
 # print(current_time) to get current time when needed
+
+
+
