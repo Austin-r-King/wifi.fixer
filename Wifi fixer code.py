5464f1c249c3b0e1664e08ec1934db9a8dd2e168
Austin-r-King <de.king.hi@gmail.com>
2021-03-17 08:56:14 -0400

    main python file

--- Wifi fixer code.py
+++ Wifi fixer code.py
@@ -9,23 +9,15 @@ t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 disconnect_log.append(current_time)
 
-
 # print(current_time) to get current time when needed
 
 
-def connection_1():
-    try:
-        socket.create_connection(("8.8.8.8", 443))
-    except:
-        print('no_connection')
-
 
 def connection_status():
-    a_socket = socket.socket()
     try:
-        a_socket.connection(("127.0.0.1", 80))
+        socket.socket.connect(("8.8.8.8", 443))
+        print("Connected")
     except:
         print("No connection")
 
-
-print(connection_1())
+print(connection_status())
