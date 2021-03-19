ac77876095963639da202003a39f1366d698f556
Austin-r-King <de.king.hi@gmail.com>
2021-03-19 08:45:05 -0400

    main python file

--- Wifi fixer code.py
+++ Wifi fixer code.py
@@ -4,16 +4,16 @@ import time
 import socket
 import os
 
+
 disconnect_log = []
 # gets the current time
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 disconnect_log.append(current_time)
-
-
 # print(current_time) to get current time when needed
 
 
+# checks if connected to wifi
 def connection_status():
     a_socket = socket.socket()
     try:
@@ -23,9 +23,14 @@ def connection_status():
         print("No connection")
 
 
+# resets wifi and connects
 def reconnect():
     var = os.popen("/usr/local/bin/resetinet.sh")
     print(var)
 
 
-print(reconnect())
+def GUI():
+
+
+
+    print(GUI())
