8ce539bddd349bc823801b7bd24f8c6c1dfc365d
Austin-r-King <de.king.hi@gmail.com>
2021-03-21 17:24:10 -0400

    main python file

--- Wifi fixer code.py
+++ Wifi fixer code.py
@@ -1,15 +1,20 @@
 # Austin, Aiden
 
-import time
-import socket
-import os
 
+import os
+import socket
+import sys
+import time
+
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 disconnect_log = []
 # gets the current time
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 disconnect_log.append(current_time)
+
+
 # print(current_time) to get current time when needed
 
 
@@ -30,7 +35,34 @@ def reconnect():
 
 
 def GUI():
+    class App(QWidget):
+
+        def __init__(self):
+            super().__init__()
+            self.title = 'PyQt5 messagebox'
+            self.left = 10
+            self.top = 10
+            self.width = 320
+            self.height = 200
+            self.initUI()
+
+        def initUI(self):
+            self.setWindowTitle(self.title)
+            self.setGeometry(self.left, self.top, self.width, self.height)
+
+            buttonReply = QMessageBox.question(self, 'Wifi reset', "Do you want to reconnect to wifi?",
+                                               QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if buttonReply == QMessageBox.Yes:
+                print('Yes')
+            else:
+                print('No')
+
+            self.show()
+
+    if __name__ == '__main__':
+        app = QApplication(sys.argv)
+        ex = App()
+        sys.exit(app.exec_())
 
 
-
-    print(GUI())
+print(GUI())
